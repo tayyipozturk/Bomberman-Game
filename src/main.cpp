f55607fa64def864a5e01222f352e5a116b49ea9
tayyipozturk <tayyip.ozturk@hotmail.com>
2023-04-12 20:47:34 +0300

    fix polling

--- src/main.cpp
+++ src/main.cpp
@@ -28,41 +28,42 @@ int main() {
             close(fd[i][0]);
             dup2(fd[i][1], STDIN_FILENO);
             // print messages coming to the pipe
-            im incoming_message;
-            //create pollfd arrays for each pipe
-            struct pollfd pfd[bombers.size()];
-            for(int j = 0; j < bombers.size(); j++){
-                pfd[j].fd = fd[j][1];
-                pfd[j].events = POLLIN;
-                pfd[j].revents = 0;
+
             }
-            int timeout = 1000;  // timeout in milliseconds
-            while(Bomber::aliveCount > 1){
-                int num_events = poll(pfd, bombers.size(), timeout);
-                if(num_events > 0){
-                    for(int j = 0; j < bombers.size(); j++){
-                        if(pfd[j].revents & POLLIN){
-                            read_data(fd[j][1], &incoming_message);
-                            imp incoming_message_print;
-                            incoming_message_print.pid = pid;
-                            incoming_message_print.m = &incoming_message;
-                            print_output(&incoming_message_print, NULL, NULL, NULL);
-                            if (incoming_message.type == BOMBER_MOVE) {
-                                //bomber.move(incoming_message.data.target_position);
-                            }
-                            else if (incoming_message.type == BOMBER_SEE) {
-                                bomber.Vision(fd[j][1], map);
-                            }
-                            else if (incoming_message.type == BOMBER_START) {
-                                bomber.Start(fd[j][1]);
-                            }
-                            else if (incoming_message.type == BOMBER_PLANT) {
-                                //bomber.plant(incoming_message.data.bomb_info.interval, incoming_message.data.bomb_info.radius);
-                            }
-                            else if (incoming_message.type == BOMB_EXPLODE) {
-                                //bomber.explode();
-                            }
-                        }
+        }
+    im incoming_message;
+    //create pollfd arrays for each pipe
+    struct pollfd pfd[bombers.size()];
+    for(int j = 0; j < bombers.size(); j++){
+        pfd[j].fd = fd[j][1];
+        pfd[j].events = POLLIN;
+        pfd[j].revents = 0;
+    }
+    int timeout = 1000;  // timeout in milliseconds
+    while(Bomber::aliveCount > 1){
+        int num_events = poll(pfd, bombers.size(), timeout);
+        if(num_events > 0){
+            for(int j = 0; j < bombers.size(); j++){
+                if(pfd[j].revents & POLLIN){
+                    read_data(fd[j][1], &incoming_message);
+                    imp incoming_message_print;
+                    incoming_message_print.pid = getpid();
+                    incoming_message_print.m = &incoming_message;
+                    print_output(&incoming_message_print, NULL, NULL, NULL);
+                    if (incoming_message.type == BOMBER_MOVE) {
+                        //bomber.move(incoming_message.data.target_position);
+                    }
+                    else if (incoming_message.type == BOMBER_SEE) {
+                        bombers[j].Vision(fd[j][1], map);
+                    }
+                    else if (incoming_message.type == BOMBER_START) {
+                        bombers[j].Start(fd[j][1]);
+                    }
+                    else if (incoming_message.type == BOMBER_PLANT) {
+                        //bomber.plant(incoming_message.data.bomb_info.interval, incoming_message.data.bomb_info.radius);
+                    }
+                    else if (incoming_message.type == BOMB_EXPLODE) {
+                        //bomber.explode();
                     }
                 }
             }
